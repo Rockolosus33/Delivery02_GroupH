a826c52d8ac8786ab97f2394c552c76dbe2d69cb
Joel Iglesias <joeltorminecraft@gmail.com>
2025-02-12 11:00:39 +0100

    Animations fixet
    I have fixed the player animations.

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -1,31 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.XR.Haptics;
 
 public class PlayerAnimation : MonoBehaviour
 {
     private Animator _animator;
     private PlayerMovement _movement;
 
+    private PlayerAnimationsStates _currentAnimation;
+
+    enum PlayerAnimationsStates
+    {
+        PlayerLeftAnimation,
+        PlayerRightAnimation,
+        PlayerFrontAnimation,
+        PlayerBackAnimation
+    }
+
     void Start()
     {
         _animator = GetComponent<Animator>();
         _movement = GetComponent<PlayerMovement>();
+        _currentAnimation = PlayerAnimationsStates.PlayerFrontAnimation;
+        _animator.SetInteger("numAnim", 2);
     }
 
     void Update()
     {
-        if (_movement._lookDirection.x < 0 && (_movement._lookDirection.y < 0 || _movement._lookDirection.y > 0) 
-            || _movement._lookDirection.x > 0 && (_movement._lookDirection.y < 0 || _movement._lookDirection.y > 0))
-        {
-            _animator.SetBool("isInDiagonal", true);
-        }
-        else
-        {
-            _animator.SetBool("isInDiagonal", false);
-        }
+        Vector2 lookDirection = _movement._lookDirection;
 
-        _animator.SetFloat("directionX", _movement._lookDirection.x);
-        _animator.SetFloat("directionY", _movement._lookDirection.y);
+        if (lookDirection.x < 0 && _currentAnimation != PlayerAnimationsStates.PlayerLeftAnimation && lookDirection.y == 0)
+        {
+            ChangeAnimation(PlayerAnimationsStates.PlayerLeftAnimation, 0);
+        }
+        else if (lookDirection.x > 0 && _currentAnimation != PlayerAnimationsStates.PlayerRightAnimation && lookDirection.y == 0)
+        {
+            ChangeAnimation(PlayerAnimationsStates.PlayerRightAnimation, 1);
+        }
+        else if (lookDirection.y < 0 && _currentAnimation != PlayerAnimationsStates.PlayerFrontAnimation)
+        {
+            ChangeAnimation(PlayerAnimationsStates.PlayerFrontAnimation, 2);
+        }
+        else if (lookDirection.y > 0 && _currentAnimation != PlayerAnimationsStates.PlayerBackAnimation)
+        {
+            ChangeAnimation(PlayerAnimationsStates.PlayerBackAnimation, 3);
+        }
+    }
+
+    void ChangeAnimation(PlayerAnimationsStates newAnimation, int number)
+    {
+        _currentAnimation = newAnimation;
+        _animator.SetInteger("numAnim", number);
+        _animator.SetTrigger("runAnimation");
     }
 }
